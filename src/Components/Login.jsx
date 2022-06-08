f484eb232596b9be2f6ca2fc7f7b1c2962ce7dc1
CHLOE JAGER <cxjmeister@yahoo.com>
2022-06-08 11:19:26 -0700

    Imported changes from previous repository.

---
+++ src/Components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes, Link, Navigate} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+const Login = (props) => {
+
+    return (
+        <section>
+            <h1>Welcome to Pinepple Kingdom</h1>
+                <input type='text' onChange={props.onChange[0]} placeholder='Enter New User Name' value={props.value[0]}/>
+                <button type='submit' onClick={props.onClick[0]}>Create New User</button>
+                <input type='text' onChange={props.onChange[1]} placeholder='User Name' value={props.value[1]}/>
+                <button type='submit' onClick={props.onClick[1]}>Login</button>
+
+
+        </section>
+    )
+}
+
+export default Login
---
+++ src/Routes/Home.js
@@ -0,0 +1,12 @@
+const Home = (props) => {
+
+    console.log(props)
+
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
