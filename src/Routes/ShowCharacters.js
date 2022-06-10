164328d040a160c1aa223d054559cfc8d044d193
CHLOE JAGER <cxjmeister@yahoo.com>
2022-06-09 22:14:54 -0700

    App.js and ShowCharacaters edited by David.

--- src/Routes/ShowCharacters.js
+++ src/Routes/ShowCharacters.js
@@ -4,17 +4,29 @@ import { useState, useEffect } from 'react'
 
 function ShowCharacters(props) {
     let user = props.user
+    let currentCharacter = props.currentCharacter
+    let setCurrentCharacter = props.setCurrentCharacter
     console.log(user)
 
+    let Character = (index) => {
+        setCurrentCharacter(user.data[0].characters[index])
+        console.log(currentCharacter)
+    }
+
+    console.log(currentCharacter)
+
     let characters
-    if (user.characters.length > 0) {
-        characters = user.characters.map((value, index) => {
-            console.log(value.class)
+
+    if (user.data.length > 0 && user.data[0].characters.length > 0) {
+        characters = user.data[0].characters.map((value, index) => {
+
             return(
                 <div key={index}>
                     <h1 className='character-display' >{value.characterName}</h1>
                     <h2 className= 'character-display' >{value.class}</h2>
-                    
+
+                    <button onClick={()=> {Character(index)}}>Want {value.characterName}?</button>
+
                  </div>
                  )
         })
@@ -23,8 +35,6 @@ function ShowCharacters(props) {
             <Link to='/home/create'><button><h2>You don't have any characters yet Create some!</h2></button></Link>
         )
     }
-
-    console.log(characters[0])
     return(
         <div>
         {characters}
